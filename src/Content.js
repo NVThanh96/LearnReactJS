0c76158bd3c65b0395e139629e86e343ee6c60b8
Nguyen Van Thanh <thanhnv@domain.vnext.vn>
2023-10-30 13:33:08 +0700

    use state Hook

--- src/Content.js
+++ src/Content.js
@@ -1,41 +1,40 @@
-import React from 'react'
+import { useState } from "react";
 
 const Content = () => {
+
+  // Thiết lập tên mặt định hiện thị sẽ là Thanh
+  const [name, setName] = useState("Thanh");
+
+  const [count, setCount] = useState(0);
+
     const handleNameChange = () => {
         const names = ['Thành', 'Luyến', 'Chip'];
         const int = Math.floor(Math.random() * 3);
-        return names[int];
+        setName(names[int]); // gắn setName ở trên vào khi click và set onClick={handleNameChange} vào button 
       }
 
-      const handleClick = () => {
-        console.log("you click it");
+      const handleClickCount = () => {
+        setCount(count + 1 );
       }
-      
-      const handleClick2 = (name) => {
-        console.log(`${name} click it`);
-      }
-      
-      // lấy giá trị của nút 
-      const handleClick3 = (e) => {
-        console.log(e.target.innerText);
-      }
-
-
 
   return (
     <main>
+
         <p>
-            hello {handleNameChange()}
+          {/* name sẽ được thay đổi theo const names = ['Thành', 'Luyến', 'Chip']; khi được vào click vào button Change Name*/}
+            hello {name}
         </p>
-        <button onClick={handleClick}>
-            Click it
+        <button onClick={handleNameChange}>
+            Change Name
         </button>
-        <button onClick={ () => handleClick2(handleNameChange())}>
-            Click
+        
+        <p>
+            Count {count}
+        </p>
+        <button onClick={handleClickCount}>
+            Count Click
         </button>
-        <button onClick={ (e) => handleClick3(e)}>
-            Click this
-        </button>
+
     </main>
   )
 }
